56e9669990bc54a722f13f6dfa1ebe482de5ef76
Arijit-mohapatra <arijitmohapatra2004@gmail.com>
2024-07-22 00:01:05 +0530

    todo app created

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TItle = () => {
+    return (
+        <div className="title">
+            <h2>todo list Arijit</h2>
+            <div className="underline"></div>
+        </div>
+    )
+}
+
+export default TItle
